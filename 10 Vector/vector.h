914d88ac35e5ddf16315644bae4375b33e455375
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-12 21:59:02 +0100

    Add files via upload

---
+++ 10 Vector/vector.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct{
+    int *tablica;
+    int capacity;
+    int numOfElem;
+}Vector;
+
+Vector *reserve(Vector *vect);   //alokowanie nowej tablicy z przepisaniem
+int size(Vector *vect);          //sprawdzanie ilosci elementow
+int capacity(Vector *vect);      //ilość zaalokowanej przestrzeni
+void empty(Vector *vect);        //sprawdza czy lista jest pusta
+void push_back(Vector *vect);    //dodawanie elementu na koniec tablicy
+void insert(Vector *vect);       //dodawanie elementu w dowolnym miejscu
+void removeElement(Vector *vect);//usuwanie elementu
+void reverse(Vector *vect);      //odwrócenie tablicy
+void at(Vector *vect);           //zwracanie N-tego elementu
+
+#endif
