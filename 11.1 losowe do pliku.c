b36452b4789191d3d2dfa7dd6766c76651e7e9a4
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 16:52:04 +0100

    Update 11.1 losowe do pliku.c

--- 11.1 losowe do pliku.c
+++ 11.1 losowe do pliku.c
@@ -4,23 +4,23 @@
 
 int main(){
     FILE *out;
-    int i,n;
+    int n;
     char filename[20];
     srand(time(0));
     printf("Program generuje wybrana ilosc liczb calkowitych i zapisuje je do wybranego pliku\n");
     printf("Podaj ilosc liczb do wygenerowania: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     if (n<1){
         printf("ERROR\nWprowadzona bledna wartosc\n");
     }
     else{
         printf("Podaj nazwe pliku: ");
-        scanf("%s",filename);
-        out=fopen(filename,"w");
+        scanf("%s", filename);
+        out=fopen(filename, "w");
 
         if (out) {
-        for(i=0;i<n;++i) {
-            fprintf(out,"%d\n",rand()%1000);
+        for(int i=0; i<n; ++i) {
+            fprintf(out, "%d\n", rand()%1000);
         }
         printf("Dane zostaly zapisane w wybranym pliku.");
         fclose(out);
