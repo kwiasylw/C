3bad0a53fe348816b0242392e275d93ef2a01995
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 17:02:20 +0100

    Update 7.1 srednia.c

--- 7.1 srednia.c
+++ 7.1 srednia.c
@@ -2,38 +2,38 @@
 
 
 void losowanieWartosci(int n, int tab[n]){
-    int i,j;
+    int j;
     srand(time(NULL));
-    for(i=0;i<n;i++){
+    for(int i=0; i<n; i++){
         tab[i]=rand()%30;
     }
 }
 
-void wyswietlZakres(int*od, int*doo){
-    for(;od<doo;++od){
-        printf("%d ",*od);
+void wyswietlZakres(int *od, int *doo){
+    for(; od<doo; ++od){
+        printf("%d ", *od);
     }
 }
 
-void srednia(int*od, int*doo){ //waga kazdego elementu to jego indeks w tablicy zaczynajac od 0
-    double licznik=0,mianownik=0,wynik;
+void srednia(int *od, int *doo){ //waga kazdego elementu to jego indeks w tablicy zaczynajac od 0
+    double licznik=0, mianownik=0, wynik;
     int waga=0;
-    for(;od<doo;++od){
+    for(; od<doo; ++od){
         licznik+=(*od)*waga;
         mianownik+=waga;
         ++waga;
     }
     wynik=licznik/mianownik;
-    printf("\nSrednia wazona elementow tablicy wynosi: %.2lf\n\n",wynik);
+    printf("\nSrednia wazona elementow tablicy wynosi: %.2lf\n\n", wynik);
 }
 
 int main(void){
     int tab[10];
-    losowanieWartosci(10,tab);
+    losowanieWartosci(10, tab);
     int *wsk=tab;
     const int N=sizeof(tab)/sizeof(tab[0]);
     printf("Tablica: ");
-    wyswietlZakres(tab,tab+N);
-    srednia(tab,tab+N);
+    wyswietlZakres(tab, tab+N);
+    srednia(tab, tab+N);
     return 0;
 }
