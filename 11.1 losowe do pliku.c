0c658d4cd6e6427a2f621d5f233c198117b877e8
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 22:24:03 +0100

    Update 11.1 losowe do pliku.c

--- 11.1 losowe do pliku.c
+++ 11.1 losowe do pliku.c
@@ -2,11 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+void generowanieDoPliku(int n){
     FILE *out;
-    int n;
     char filename[20];
-    srand(time(0));
+    srand(time(NULL));
+    printf("Podaj nazwe pliku: ");
+    scanf("%s", filename);
+    out=fopen(filename, "w");
+    if (out){
+        for(int i=0; i<n; ++i){
+            fprintf(out, "%d\n", rand()%1000);
+        }
+        printf("Dane zostaly zapisane w wybranym pliku.\n");
+        fclose(out);
+    }
+    else{
+        printf("ERROR\nWprowadzona bledna nazwe\n");
+        exit(1);
+    }
+}
+
+int main(){
+    int n;
     printf("Program generuje wybrana ilosc liczb calkowitych i zapisuje je do wybranego pliku\n");
     printf("Podaj ilosc liczb do wygenerowania: ");
     scanf("%d", &n);
@@ -14,21 +31,7 @@ int main(){
         printf("ERROR\nWprowadzona bledna wartosc\n");
     }
     else{
-        printf("Podaj nazwe pliku: ");
-        scanf("%s", filename);
-        out=fopen(filename, "w");
-
-        if (out) {
-        for(int i=0; i<n; ++i) {
-            fprintf(out, "%d\n", rand()%1000);
-        }
-        printf("Dane zostaly zapisane w wybranym pliku.");
-        fclose(out);
-        }
-        else{
-            printf("ERROR\nWprowadzona bledna nazwe\n");
-            exit(1);
-        }
+        generowanieDoPliku(n);
     }
     return 0;
 }
