2d5bb03fedb10b04b268a849d482bb9e77c073f7
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 21:48:31 +0100

    Update 11.2 sortowanie do pliku.c

--- 11.2 sortowanie do pliku.c
+++ 11.2 sortowanie do pliku.c
@@ -17,8 +17,8 @@ void sortowanie(int n, int tab[]){
 
 int main(){
     FILE *in, *out;
-    int i=0,totalNum,tab[100];
-    char line[100],filename1[20],filename2[20];
+    int i=0, totalNum, tab[100];
+    char line[100], filename1[20], filename2[20];
     printf("Program pobiera liczby z I programuje, sortuje je i zapisuje do II pliku.\n\n");
     printf("Podaj nazwe pliku do pobrania danych: ");
     scanf("%s", filename1);
@@ -32,7 +32,7 @@ int main(){
             i++;
         }
         totalNum=i;
-        sortowanie(totalNum,tab);
+        sortowanie(totalNum, tab);
         fprintf(out,"Posortowane liczby:\n");
         for(i=0; i<totalNum; ++i){
             fprintf(out, "%d\n", tab[i]);
