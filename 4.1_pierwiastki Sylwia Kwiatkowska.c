4e772b088a4333355d0f74049716617e1b8c0769
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 14:41:36 +0100

    Update 4.1_pierwiastki Sylwia Kwiatkowska.c

--- 4.1_pierwiastki Sylwia Kwiatkowska.c
+++ 4.1_pierwiastki Sylwia Kwiatkowska.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
-{
-    printf("Program liczy pierwiastki rownania kwadratowego.\n\n");
-    float delta,pierwiastek,x1,x2;
-    int a,b,c;
-    printf("Podaj wartosci wspolczynnikow: ax^2+bx+c \n");
-    printf("\ta=");
-    scanf("%d",&a);
-    printf("\tb=");
-    scanf("%d",&b);
-    printf("\tc=");
-    scanf("%d",&c);
+float liczenieDelty(int a, int b, int c){
+    float delta=b*b-4.*a*c;
+    float pierwiastek,x1,x2;
+    printf("\nDelta wynosi: %.1f \n", delta);
+    if (delta<0){
+        printf("\nBrak rozwiazan\n");
+    }
+    if(delta==0){
+        x1=-b/2*a;
+        printf("Pierwiastek rownania: %.1f\n",x1);
+    }
+    if (delta>0){
+        pierwiastek=sqrt(delta);
+        x1=(-b-pierwiastek)/2*a;
+        x2=(-b+pierwiastek)/2*a;
+        printf("\nPierwiastki rownania to %.1f, %.1f \n",x1,x2);
+    }
+}
 
+int main(void){
+    printf("Program liczy pierwiastki rownania kwadratowego.\n\n");
+    int a,b,c;
+    printf("Podaj wartosci wspolczynnikow: ax^2+bx+c (oddzielone spacjami)\n");
+    printf("Wspolczynniki a b c : ");
+    scanf("%d %d %d",&a,&b,&c);
     if (a==0){
         printf("Wspolczynnik a musi byc rozny od zera!!\n");
     }
-
     else{
-        delta=b*b-4.*a*c;
-        printf("\nDelta wynosi: %.1f \n", delta);
-
-        if (delta<0){
-            printf("\nBrak rozwiazan\n");
-        }
-
-        if(delta==0){
-            x1=-b/2*a;
-            printf("Pierwiastek rownania: %.1f\n",x1);
-        }
-
-        if (delta>0){
-            pierwiastek=sqrt(delta);
-            x1=(-b-pierwiastek)/2*a;
-            x2=(-b+pierwiastek)/2*a;
-            printf("\nPierwiastki rownania to %.1f, %.1f \n",x1,x2);
-        }
+        liczenieDelty(a,b,c);
     }
-
-return 0;
+    return 0;
 }
