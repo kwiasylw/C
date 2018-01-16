ad2d257eec751dd2027f535ae28e37d21c11c269
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 18:36:19 +0100

    Update 6.1 ascii.c

--- 6.1 ascii.c
+++ 6.1 ascii.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
+#define ZAKRESMAX 255
 
-int main()
-{
-    int i;
+int main(void){
     puts("Program wypisuje znaki ASCII od 0 do 255");
-    for(i=0; i<=255; i++)
-    {
+    for(int i=0; i<=ZAKRESMAX; i++){
         printf("%c ma kod ASCII: %d\n", i, i);
     }
-
     return 0;
 }
