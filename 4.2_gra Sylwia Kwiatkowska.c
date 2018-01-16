d9c34710c52df01ea07e0402f2e21911775ddeec
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 15:14:32 +0100

    Update 4.2_gra Sylwia Kwiatkowska.c

--- 4.2_gra Sylwia Kwiatkowska.c
+++ 4.2_gra Sylwia Kwiatkowska.c
@@ -2,35 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
+const int zakresOd=1, zakresDo=10;
+
+int sprawdzenieIKorygowanieLiczby(int liczba, int zakresDo, int zakresOd){
+    if ((liczba>zakresDo) || (liczba<zakresOd)){
+            printf("*\n*\nWpisales liczbe spoza zakresu!!\n");
+            printf("Twoja liczba zostanie zmodyfikowana, aby nalezala do danego zakresu\n");
+            liczba=(abs(liczba))%10;
+            printf("*\n*\nTeraz Twoja liczba to: %d\n\n", liczba);
+        }
+    return liczba;
+}
+
 int main(void){
     srand(time(NULL));
     int randomowa=rand()%10+1;
-    int poprawne=0,licznik=0,liczbaUzytk;
-    int zakresOd=1,zakresDo=10;
+    int poprawne=0, liczbaProbGracza=0, liczbaWybranaPrzezGracza;
     printf("Zgadnij liczbe wylosowana przez komputer! (Zakres od 1 do 10) ");
     printf("\nWpisz liczbe: ");
     do{
-        scanf("%d", &liczbaUzytk);
-        if ((liczbaUzytk>zakresDo)||(liczbaUzytk<zakresOd)){
-            printf("*\n*\nWpisales liczbe spoza zakresu!!\n");
-            printf("Twoja liczba zostanie zmodyfikowana, aby nalezala do danego zakresu\n");
-            liczbaUzytk=(abs(liczbaUzytk))%10;
-            printf("*\n*\nTeraz Twoja liczba to: %d\n\n",liczbaUzytk);
-        }
-        if (liczbaUzytk==randomowa){
-            licznik++;
-            printf("\nMasz racje! Komputer wylosowal liczbe %d. \nZgadles za %d razem, gratulacje!\n",randomowa,licznik);
-            poprawne=1;
+        scanf("%d", &liczbaWybranaPrzezGracza);
+        liczbaWybranaPrzezGracza=sprawdzenieIKorygowanieLiczby(liczbaWybranaPrzezGracza, zakresDo, zakresOd);
+        if (liczbaWybranaPrzezGracza==randomowa){
+            printf("\nMasz racje! Komputer wylosowal liczbe %d. \nZgadles za %d razem, gratulacje!\n", randomowa, liczbaProbGracza+1);
             break;
         }
-        if (liczbaUzytk<randomowa){
-            licznik++;
+        if (liczbaWybranaPrzezGracza<randomowa){
             printf("\nWpisales za mala liczbe. Sprobuj jeszcze raz:  ");
         }
-        if (liczbaUzytk>randomowa){
-            licznik++;
+        if (liczbaWybranaPrzezGracza>randomowa){
             printf("\nWpisales za duza liczbe. Sprobuj jeszcze raz:  ");
         }
+        liczbaProbGracza++;
     } while (poprawne==0);
     return 0;
 }
