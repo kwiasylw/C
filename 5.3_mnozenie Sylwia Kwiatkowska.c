64f8c41d2d93ee156d9611c55d9965c05f5e5426
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 14:42:53 +0100

    Update 5.3_mnozenie Sylwia Kwiatkowska.c

--- 5.3_mnozenie Sylwia Kwiatkowska.c
+++ 5.3_mnozenie Sylwia Kwiatkowska.c
@@ -2,65 +2,54 @@
 #include <stdlib.h>
 #include <time.h>
 
-void macierz2x2Na2x2x2(int macierz1[2][2], int macierz2[2][2],int macierz3[2][2][2])
-{
-    int i,j;
-    for (i=0;i<2;i++){
-        for (j=0;j<2;j++){
+void losowanieWartosci(int n, int k, int macierz1[n][k], int macierz2[n][k]){
+    srand(time(NULL));
+    for (int i=0;i<n;i++){
+        for (int j=0;j<k;j++){
+            macierz1[i][j]=(-100)+rand()%200;
+            macierz2[i][j]=(-100)+rand()%200;
+        }
+    }
+}
+
+void dwieMacierze2x2NaJednaMacierz2x2x2(int n, int k, int l, int macierz1[n][k], int macierz2[n][k],int macierz3[n][k][l]){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<k;j++){
             macierz3[0][i][j]=macierz1[i][j];
             macierz3[1][i][j]=macierz2[i][j];
         }
     }
 }
 
-void macierz2x2x2_to_macierz2x2(int macierz3[2][2][2],int macierz1[2][2],int macierz2[2][2]){
-    int i,j;
-    for (i=0;i<2;i++){
-        for (j=0;j<2;j++){
+void jednaMacierz2x2x2NaDwieMacierze2x2(int n, int k, int l, int macierz3[n][k][l], int macierz1[n][k], int macierz2[n][k]){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<k;j++){
             macierz1[i][j]=macierz3[0][i][j];
             macierz2[i][j]=macierz3[1][i][j];
         }
     }
 }
 
-void mnozenie(int macierz[2][2][2]){
-    int macierz1[2][2];
-    int macierz2[2][2];
-    macierz2x2x2_to_macierz2x2(macierz,macierz1,macierz2);
-    int i,j;
-    int wynikJako2x2[2][2];
-
-    printf("W wyniku mnozenia losowych macierzy powstaje macierz:\n\n");
-    for (i=0;i<2;i++){
+void mnozenieOrazWypisywanieMacierzyWynikowej (int n, int k, int l, int macierz[n][k][l]){
+    int macierz1[n][k], macierz2[n][k], wynikJako2x2[n][k];
+    jednaMacierz2x2x2NaDwieMacierze2x2(2,2,2,macierz,macierz1,macierz2);
+    for (int i=0;i<n;i++){
         wynikJako2x2[0][i]= macierz1[0][0]*macierz2[0][i] + macierz1[0][1]*macierz2[1][i];
         printf("%d\t", wynikJako2x2[0][i]);
     }
     printf("\n");
-    for (j=0;j<2;j++){
+    for (int j=0;j<k;j++){
         wynikJako2x2[1][j]= macierz1[1][0]*macierz2[0][j]+ macierz1[1][1]*macierz2[1][j];
         printf("%d\t", wynikJako2x2[1][j]);
     }
 }
 
-
-int main(int argc, char *argv[])
-{
-    srand(time(NULL));
-    int i,j;
-    int macierz3[2][2][2];
-    int macierz1[2][2];
-    int macierz2[2][2];
-
-    for (i=0;i<2;i++){
-        for (j=0;j<2;j++){
-            macierz1[i][j]=(-100)+rand()%200;
-            macierz2[i][j]=(-100)+rand()%200;
-        }
-    }
-
-    macierz2x2Na2x2x2(macierz1,macierz2,macierz3);
-    mnozenie(macierz3);
-
-printf("\n\n");
-return 0;
+int main(){
+    int macierz3[2][2][2], macierz1[2][2], macierz2[2][2];
+    losowanieWartosci(2,2,macierz1,macierz2);
+    dwieMacierze2x2NaJednaMacierz2x2x2(2,2,2,macierz1,macierz2,macierz3);
+    printf("W wyniku mnozenia losowych macierzy powstaje macierz:\n\n");
+    mnozenieOrazWypisywanieMacierzyWynikowej(2,2,2,macierz3);
+    printf("\n\n");
+    return 0;
 }
