1124403c7989f341cd1d46e4c6e74d7353390668
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 14:43:45 +0100

    Update 7.1 srednia.c

--- 7.1 srednia.c
+++ 7.1 srednia.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
+
+void losowanieWartosci(int n, int tab[n]){
+    int i,j;
+    srand(time(NULL));
+    for(i=0;i<n;i++){
+        tab[i]=rand()%30;
+    }
+}
+
 void wyswietlZakres(int*od, int*doo){
-    for(; od<doo;++od){
-        printf("%d ", *od);
+    for(;od<doo;++od){
+        printf("%d ",*od);
     }
 }
 
 void srednia(int*od, int*doo){ //waga kazdego elementu to jego indeks w tablicy zaczynajac od 0
-    double licznik=0;
-    double mianownik=0;
+    double licznik=0,mianownik=0,wynik;
     int waga=0;
-    double wynik;
-    for(; od<doo;++od){
+    for(;od<doo;++od){
         licznik+=(*od)*waga;
         mianownik+=waga;
         ++waga;
@@ -21,17 +28,12 @@ void srednia(int*od, int*doo){ //waga kazdego elementu to jego indeks w tablicy 
 }
 
 int main(void){
-    int  i,tab[10];
-    srand(time(NULL));
-    for(i=0;i<10;i++){
-    tab[i]=rand()%30;
-    }
+    int tab[10];
+    losowanieWartosci(10,tab);
     int *wsk=tab;
     const int N=sizeof(tab)/sizeof(tab[0]);
     printf("Tablica: ");
     wyswietlZakres(tab,tab+N);
     srednia(tab,tab+N);
-
-
-        return 0;
+    return 0;
 }
