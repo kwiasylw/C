2ce09f769dc56a7df1d4ee1df67e359e418f7d56
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 21:47:11 +0100

    Update 7.3 sort tab.c

--- 7.3 sort tab.c
+++ 7.3 sort tab.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
+#define ILOSCELEMENTOW 10
 
 int main(void) {
     int tab[10];
     srand(time(NULL));
-    for(int i=0; i<10; i++){
+    for (int i=0; i<ILOSCELEMENTOW; i++){
         tab[i]=rand()%30;
     }
     int *wsk[10];
     puts("***POCZATKOWO***");
     puts("L.P.\ttab A\ttab B");
     puts("---------------------");
-    for (int j=0; j<10; j++){
+    for (int j=0; j<ILOSCELEMENTOW; j++){
         wsk[j]=&tab[j];
     }
-    for (int j=0; j<10; j++){
+    for (int j=0; j<ILOSCELEMENTOW; j++){
         printf("%d\t%d\t%d\n", j, tab[j], *wsk[j]);
     }
 
     int *tmp;
     //sortowanie
-    for (int k=0; k<10; k++){
-        for (int j=0; j<9; j++){
+    for (int k=0; k<ILOSCELEMENTOW; k++){
+        for (int j=0; j<ILOSCELEMENTOW-1; j++){
             if (*wsk[j]>*wsk[j+1]){
                 tmp=wsk[j+1];
                 wsk[j+1]=wsk[j];
@@ -35,7 +37,7 @@ int main(void) {
     puts("***REZULTAT KONCOWY***");
     puts("L.P.\ttab A\ttab B");
     puts("---------------------");
-    for (int j=0; j<10; j++){
+    for (int j=0; j<ILOSCELEMENTOW; j++){
         printf("%d\t%d\t%d\n", j, tab[j], *wsk[j]);
    }
     return 0;
