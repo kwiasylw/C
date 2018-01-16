7cd6adc9cd7b4e1d9d77d543f3fd7c1d5da465e6
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 14:40:41 +0100

    Update 3.2_temp Sylwia Kwiatkowska.c

--- 3.2_temp Sylwia Kwiatkowska.c
+++ 3.2_temp Sylwia Kwiatkowska.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 
-int main()
-{
-    float tempF,tempC;
+void fahrenheityNaCelsjusze(void){
+    float tempWFahrenheitach, tempWCelsjuszach;
+    printf("Wpisz temperature w stopniach Fahrenheita: ");
+    scanf("%f",&tempWFahrenheitach);
+    tempWCelsjuszach=(tempWFahrenheitach-32)/1.8;
+    printf("Temperatura w stopniach Celsjusza wynosi: %.2f\n", tempWCelsjuszach);
+}
+
+void celsjuszeNaFahrenheity(void){
+    float tempWCelsjuszach, tempWFahrenheitach;
+    printf("Wpisz temperature w stopniach Celsjusza: ");
+    scanf("%f",&tempWCelsjuszach);
+    tempWFahrenheitach=(tempWCelsjuszach*1.8)+32;
+    printf("Temperatura w stopniach Fahrenheita wynosi: %.2f\n", tempWFahrenheitach);
+}
+
+int main(){
     int wybor;
     printf("Program przelicza temperature w skalach stopnie Celsjusza <-> stopnie Fahrenheita\n\n");
     printf("\t1: stopnie Fahrenheita -> stopnie Celsjusza\n");
     printf("\t2: stopnie Celsiusza -> Fahrenheita\n");
     printf("\nWpisz wybor (1 lub 2): ");
     scanf("%d",&wybor);
-
     if(wybor==1){
-        printf("Wpisz temperature w stopniach Fahrenheita: ");
-        scanf("%f",&tempF);
-        tempC= (tempF - 32) / 1.8;
-        printf("Temperatura w stopniach Celsjusza wynosi: %.2f\n",tempC);
+        fahrenheityNaCelsjusze();
     }
-
     else if(wybor==2){
-        printf("Wpisz temperature w stopniach Celsjusza: ");
-        scanf("%f",&tempC);
-        tempF=(tempC*1.8)+32;
-        printf("Temperatura w stopniach Fahrenheita wynosi: %.2f\n",tempF);
+        celsjuszeNaFahrenheity();
     }
-
     else{
         printf("\nZly wybor");
     }
-
-return 0;
+    return 0;
 }
