4ba76bb83b85e7fe1a6219002bb7f7181c4106e0
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 16:55:48 +0100

    Update 7.3 sort tab.c

--- 7.3 sort tab.c
+++ 7.3 sort tab.c
@@ -3,27 +3,26 @@
 
 
 int main(void) {
-    int i,tab[10];
+    int tab[10];
     srand(time(NULL));
-    for(i=0;i<10;i++){
+    for(int i=0; i<10; i++){
         tab[i]=rand()%30;
     }
-    int j,*wsk[10];
+    int *wsk[10];
     puts("***POCZATKOWO***");
     puts("L.P.\ttab A\ttab B");
     puts("---------------------");
-    for (j=0;j<10; j++){
+    for (int j=0; j<10; j++){
         wsk[j]=&tab[j];
     }
-    for (j=0;j<10;j++){
-        printf("%d\t%d\t%d\n",j,tab[j],*wsk[j]);
+    for (int j=0; j<10; j++){
+        printf("%d\t%d\t%d\n", j, tab[j], *wsk[j]);
     }
 
     int *tmp;
-    int k;
     //sortowanie
-    for (k=0;k<10;k++){
-        for (j=0;j<9;j++){
+    for (int k=0; k<10; k++){
+        for (int j=0; j<9; j++){
             if (*wsk[j]>*wsk[j+1]){
                 tmp=wsk[j+1];
                 wsk[j+1]=wsk[j];
@@ -36,8 +35,8 @@ int main(void) {
     puts("***REZULTAT KONCOWY***");
     puts("L.P.\ttab A\ttab B");
     puts("---------------------");
-    for (j=0;j<10;j++){
-        printf("%d\t%d\t%d\n",j,tab[j],*wsk[j]);
+    for (int j=0; j<10; j++){
+        printf("%d\t%d\t%d\n", j, tab[j], *wsk[j]);
    }
     return 0;
 }
