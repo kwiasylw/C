b3df60a1c099ee3af91624bcaae1c1a0cdf06d89
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 14:42:21 +0100

    Update 5.1_czasy Sylwia Kwiatkowska.c

--- 5.1_czasy Sylwia Kwiatkowska.c
+++ 5.1_czasy Sylwia Kwiatkowska.c
@@ -1,69 +1,66 @@
 #include <stdio.h>
 
+enum IndeksCzasu{
+    INDEKS_GODZINY=0,
+    INDEKS_MINUTY=1,
+    INDEKS_SEKUNDY=2
+    };
 
-int porownywanieCzasow(int czas1[], int czas2[]){
-    int i,j,godzina1,godzina2,minuta1,minuta2,sekunda1,sekunda2;
-    godzina1=czas1[0];
-    godzina2=czas2[0];
-    minuta1=czas1[1];
-    minuta2=czas2[1];
-    sekunda1=czas1[2];
-    sekunda2=czas2[2];
 
-    if ((godzina2<=23) && (godzina1<=23)
-        && (minuta1<=59) && (minuta2<=59) && (sekunda1=59) && (sekunda2<=59)
-        && (godzina1>=0) && (godzina2>=0) && (sekunda1>=0) && (sekunda2>=0) && (minuta1>=0) && (minuta2>=0)){
-
-            for (i=0;i<3;i++){
-                if (czas1[i]>czas2[i]){
-                    j=1;
-                    break;
-                }
-
-                if (czas1[i]<czas2[i]){
-                    j=-1;
-                    break;
-                }
-
-                else{
-                    j=0;
-                }
-            }
-            printf("\n\nWYNIK: ");
-            return j;
+void podawanieDanych(int n, int czas[n]){
+    for(int i=0;i<n;++i){
+        printf("\t%d wartosc: ",i+1);
+        scanf("%d",&czas[i]);
     }
-
-    else{
-        printf("\n\nZly zapis czasu!!\n");
-        printf("ERROR ");
-        return 404;
-
-    }
-
 }
 
-int main(int argc, char *argv[]){
+int sprawdzPoprawnoscDanych(int godzina, int minuta, int sekunda){
+    if ((godzina>=0) && (godzina<=23) && (minuta>=0) && (minuta<=59) && (sekunda>=0) && (sekunda<=59)){
+        return 0;
+    }
+    else{
+        printf("\n\nZly zapis czasu.\n");
+        return 1;
+    }
+}
 
+int porownywanieCzasow(int n,int czas1[n], int czas2[n]){
+    int j;
+    if ( (sprawdzPoprawnoscDanych(czas1[INDEKS_GODZINY],czas1[INDEKS_MINUTY],czas1[INDEKS_SEKUNDY]))
+        || (sprawdzPoprawnoscDanych(czas2[INDEKS_GODZINY],czas2[INDEKS_MINUTY],czas2[INDEKS_SEKUNDY])) ){
+        return 2;
+    }
+    else{
+        for (int i=0;i<n;++i){
+            if (czas1[i]>czas2[i]){
+                j=1;
+                break;
+            }
+            if (czas1[i]<czas2[i]){
+                j=-1;
+                break;
+            }
+            else{
+                j=0;
+            }
+        }
+        return j;
+    }
+}
+
+int main(void){
     printf("Program porownuje dwie godziny\n\n");
     printf("Legenda: \n\t-1: czas pierwszy jest wczesniej \n\t 0: czasy sa rowne \n\t 1:czas drugi jest wczesniej\n\n\n");
-    int i;
-    int tablica1[3];
-    int tablica2[3];
-
+    int czas1[3], czas2[3];
+    PODAWANIE_DANYCH:
     printf("\nPodaj pierwsza godzine w formacie GG-MM-SS: \n");
-
-    for(i=0;i<3;i++){
-        printf("\t%d wartosc: ",i+1);
-        scanf("%d",&tablica1[i]);
-        }
-
+    podawanieDanych(3,czas1);
     printf("\n\nPodaj druga godzine w formacie GG-MM-SS: \n");
-    for(i=0;i<3;i++){
-        printf("\t%d wartosc: ",i+1);
-        scanf("%d",&tablica2[i]);
-        }
-
-    printf("%d",porownywanieCzasow(tablica1,tablica2));
-    printf("\n\n\n");
+    podawanieDanych(3,czas2);
+    if(porownywanieCzasow(3,czas1,czas2)==2){
+        goto PODAWANIE_DANYCH;
+    }
+    printf("\nWynik porownywania to: %d",porownywanieCzasow(3,czas1,czas2));
+    printf("\n\n");
     return 0;
 }
