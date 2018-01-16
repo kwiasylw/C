fd1b4b81b9b60a960db6980acd3e5a37dc5cd6e3
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-16 15:35:49 +0100

    Update 3.3_radian Sylwia Kwiatkowska.c

--- 3.3_radian Sylwia Kwiatkowska.c
+++ 3.3_radian Sylwia Kwiatkowska.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 float stopnieNaradianyiany(void){
     float stopnie, radiany;
     printf("\nWpisz kat w stopniach: ");
