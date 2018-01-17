dfc04854c9d0caae59cb250185b8866c394d8614
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-17 21:05:36 +0100

    Update 7.3 sort tab.c

--- 7.3 sort tab.c
+++ 7.3 sort tab.c
@@ -4,25 +4,30 @@
 
 #define ILOSCELEMENTOW 10
 
-int main(void) {
-    int tab[10];
+
+void losowanieWartosci(int tab[]){
     srand(time(NULL));
     for (int i=0; i<ILOSCELEMENTOW; i++){
         tab[i]=rand()%30;
     }
-    int *wsk[10];
-    puts("***POCZATKOWO***");
-    puts("L.P.\ttab A\ttab B");
-    puts("---------------------");
+}
+
+void przypisywanieWskaznikomOdpowiednichWartosciZTablicy(int tab[], int *wsk[]){
     for (int j=0; j<ILOSCELEMENTOW; j++){
         wsk[j]=&tab[j];
     }
+}
+
+void wypisywanieNaEkranZawartosciTablic(int tab[], int **wsk){
+    puts("L.P.\ttab A\ttab B");
+    puts("---------------------");
     for (int j=0; j<ILOSCELEMENTOW; j++){
         printf("%d\t%d\t%d\n", j, tab[j], *wsk[j]);
     }
+}
 
+void sortowanieBabelkowe(int **wsk){
     int *tmp;
-    //sortowanie
     for (int k=0; k<ILOSCELEMENTOW; k++){
         for (int j=0; j<ILOSCELEMENTOW-1; j++){
             if (*wsk[j]>*wsk[j+1]){
@@ -32,13 +37,16 @@ int main(void) {
             }
         }
     }
+}
 
-    puts("\n");
-    puts("***REZULTAT KONCOWY***");
-    puts("L.P.\ttab A\ttab B");
-    puts("---------------------");
-    for (int j=0; j<ILOSCELEMENTOW; j++){
-        printf("%d\t%d\t%d\n", j, tab[j], *wsk[j]);
-   }
+int main(void) {
+    int tab[10], *wsk[10];
+    losowanieWartosci(tab);
+    puts("***POCZATKOWO***");
+    przypisywanieWskaznikomOdpowiednichWartosciZTablicy(tab, wsk);
+    wypisywanieNaEkranZawartosciTablic(tab, wsk);
+    sortowanieBabelkowe(wsk);
+    puts("\n\n***REZULTAT KONCOWY***");
+    wypisywanieNaEkranZawartosciTablic(tab, wsk);
     return 0;
 }
