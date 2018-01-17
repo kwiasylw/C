dc8c61ce804d6455b335bb64a27253441fc28a56
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-17 14:51:18 +0100

    Update vector.h

--- vector.h
+++ vector.h
@@ -9,8 +9,8 @@ typedef struct{
 
 Vector *reserve(Vector *vect);   //alokowanie nowej tablicy z przepisaniem
 int size(Vector *vect);          //sprawdzanie ilosci elementow
-int capacity(Vector *vect);      //ilość zaalokowanej przestrzeni
-void empty(Vector *vect);        //sprawdza czy lista jest pusta
+int capacity(Vector *vect);      //iloœæ zaalokowanej przestrzeni
+int empty(Vector *vect);        //sprawdza czy lista jest pusta
 void push_back(Vector *vect);    //dodawanie elementu na koniec tablicy
 void insert(Vector *vect);       //dodawanie elementu w dowolnym miejscu
 void removeElement(Vector *vect);//usuwanie elementu
