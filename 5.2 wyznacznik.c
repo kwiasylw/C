d73ac927bf2feddcbd3998f375275206ce521a91
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-17 23:50:23 +0100

    Update 5.2 wyznacznik.c

--- 5.2 wyznacznik.c
+++ 5.2 wyznacznik.c
@@ -15,7 +15,7 @@ void wypisywanieMacierzy(int n, int a[n][n]){
     for(int i=0; i<n; ++i){
         printf("\n");
         for(int j=0; j<n; ++j){
-            printf("%d\t",a[i][j]);
+            printf("%d\t", a[i][j]);
         }
     }
 }
