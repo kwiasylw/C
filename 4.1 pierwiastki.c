70c302d27a8e5b8a40211c0887802bedc63906e6
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-24 16:22:16 +0100

    Update 4.1 pierwiastki.c

--- 4.1 pierwiastki.c
+++ 4.1 pierwiastki.c
@@ -6,14 +6,14 @@ float liczenieDelty(int a, int b, int c){
     float delta=b*b-4.*a*c;
     float pierwiastek, x1, x2;
     printf("\nDelta wynosi: %.1f \n", delta);
-    if (delta<0){
+    if(delta<0){
         printf("\nBrak rozwiazan\n");
     }
     if(delta==0){
         x1=-b/2*a;
         printf("Pierwiastek rownania: %.1f\n", x1);
     }
-    if (delta>0){
+    if(delta>0){
         pierwiastek=sqrt(delta);
         x1=(-b-pierwiastek)/2*a;
         x2=(-b+pierwiastek)/2*a;
@@ -27,7 +27,7 @@ int main(void){
     printf("Podaj wartosci wspolczynnikow: ax^2+bx+c (oddzielone spacjami)\n");
     printf("Wspolczynniki a b c : ");
     scanf("%d %d %d", &a, &b, &c);
-    if (a==0){
+    if(a==0){
         printf("Wspolczynnik a musi byc rozny od zera!!\n");
     }
     else{
