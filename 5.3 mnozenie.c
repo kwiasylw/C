bdf5e89f5334969e5dd0a2f37cca4924df84ea2b
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-24 16:24:28 +0100

    Update 5.3 mnozenie.c

--- 5.3 mnozenie.c
+++ 5.3 mnozenie.c
@@ -4,8 +4,8 @@
 
 void losowanieMacierzy(int n, int k, int macierz1[n][k], int macierz2[n][k]){
     srand(time(NULL));
-    for (int i=0; i<n; i++){
-        for (int j=0; j<k; j++){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<k; j++){
             macierz1[i][j]=(-100)+rand()%200;
             macierz2[i][j]=(-100)+rand()%200;
         }
@@ -13,8 +13,8 @@ void losowanieMacierzy(int n, int k, int macierz1[n][k], int macierz2[n][k]){
 }
 
 void dwieMacierze2x2NaJednaMacierz2x2x2(int n, int k, int l, int macierz1[n][k], int macierz2[n][k], int macierz3[n][k][l]){
-    for (int i=0; i<n; i++){
-        for (int j=0; j<k; j++){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<k; j++){
             macierz3[0][i][j]=macierz1[i][j];
             macierz3[1][i][j]=macierz2[i][j];
         }
@@ -22,8 +22,8 @@ void dwieMacierze2x2NaJednaMacierz2x2x2(int n, int k, int l, int macierz1[n][k],
 }
 
 void jednaMacierz2x2x2NaDwieMacierze2x2(int n, int k, int l, int macierz3[n][k][l], int macierz1[n][k], int macierz2[n][k]){
-    for (int i=0; i<n; i++){
-        for (int j=0; j<k; j++){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<k; j++){
             macierz1[i][j]=macierz3[0][i][j];
             macierz2[i][j]=macierz3[1][i][j];
         }
@@ -33,13 +33,13 @@ void jednaMacierz2x2x2NaDwieMacierze2x2(int n, int k, int l, int macierz3[n][k][
 void mnozenieOrazWypisywanieMacierzyWynikowej (int n, int k, int l, int macierz[n][k][l]){
     int macierz1[n][k], macierz2[n][k], wynikJako2x2[n][k];
     jednaMacierz2x2x2NaDwieMacierze2x2(2, 2, 2, macierz, macierz1, macierz2);
-    for (int i=0; i<n; i++){
+    for(int i=0; i<n; i++){
         wynikJako2x2[0][i]=macierz1[0][0]*macierz2[0][i]+macierz1[0][1]*macierz2[1][i];
         printf("%d\t", wynikJako2x2[0][i]);
     }
     printf("\n");
     
-    for (int j=0; j<k; j++){
+    for(int j=0; j<k; j++){
         wynikJako2x2[1][j]=macierz1[1][0]*macierz2[0][j]+macierz1[1][1]*macierz2[1][j];
         printf("%d\t", wynikJako2x2[1][j]);
     }
