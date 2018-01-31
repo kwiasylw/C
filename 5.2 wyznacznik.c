8967e8ac558a0295db8b382d970ec6d1739b710d
sylwiakwiatkowska1 <35047103+sylwiakwiatkowska1@users.noreply.github.com>
2018-01-31 17:56:18 +0100

    Update 5.2 wyznacznik.c

--- 5.2 wyznacznik.c
+++ 5.2 wyznacznik.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define WYMIAR 3
 
-void losowanieWartosci(int n, int a[n][n]){
+
+void losowanieWartosci(int a[WYMIAR][WYMIAR]){
     srand(time(NULL));
-    for(int i=0; i<n; ++i){
-        for(int j=0; j<n; ++j){
+    for(int i=0; i<WYMIAR; ++i){
+        for(int j=0; j<WYMIAR; ++j){
             a[i][j]=rand()%10;
         }
     }
 }
 
-void wypisywanieMacierzy(int n, int a[n][n]){
-    for(int i=0; i<n; ++i){
+void wypisywanieMacierzy(int a[WYMIAR][WYMIAR]){
+    for(int i=0; i<WYMIAR; ++i){
         printf("\n");
-        for(int j=0; j<n; ++j){
+        for(int j=0; j<WYMIAR; ++j){
             printf("%d\t", a[i][j]);
         }
     }
 }
 
-void obliczanieWyznacznika(int n, int a[n][n]){
+void obliczanieWyznacznika(int a[WYMIAR][WYMIAR]){
     int wyznacznik;
     wyznacznik=a[0][0]*a[1][1]*a[2][2]+a[0][1]*a[1][2]*a[2][0]-a[0][2]*a[1][1]*a[2][0]-a[0][0]*a[1][2]*a[2][1]-a[0][1]*a[1][0]*a[2][2];
     printf("\n\nWyznacznik tej macierzy 3x3 wynosi: %d\n\n", wyznacznik);
@@ -28,11 +30,11 @@ void obliczanieWyznacznika(int n, int a[n][n]){
 
 
 int main(){
-    int a[3][3];
+    int a[WYMIAR][WYMIAR];
     printf("Program oblicza wyznacznik macierzy 3x3 o losowo wybranych wartosciach.\n");
-    losowanieWartosci(3, a);
+    losowanieWartosci(a);
     printf("\nMacierz ma postac:");
-    wypisywanieMacierzy(3, a);
-    obliczanieWyznacznika(3, a);
+    wypisywanieMacierzy(a);
+    obliczanieWyznacznika(a);
     return 0;
 }
